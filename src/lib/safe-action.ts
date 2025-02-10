b0b8b80e7bbb30b0fdd8bdc05c6d2705b14c1b5c
Ziya <ziyaelarief@gmail.com>
2025-02-10 20:21:34 +0700

    Show error messageto users  if it's domain exception

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,7 +1,14 @@
+import { DomainException } from '@/server/domain/exceptions/DomainException';
 import { createSafeActionClient } from 'next-safe-action';
 
 export const actionClient = createSafeActionClient({
   handleServerError: (error) => {
+    if (error instanceof DomainException) {
+      return {
+        error: error.message,
+      };
+    }
+
     console.error(error);
     return {
       error: 'An unexpected error occurred',
